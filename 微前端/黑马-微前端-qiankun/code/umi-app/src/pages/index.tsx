2d2a1725a6183ff7d054b42e630e2d1b5008121f
linxiang <lionsom.linx@qq.com>
2024-02-03 14:42:31 +0800

    微前端

---
+++ 微前端/黑马-微前端-qiankun/code/umi-app/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import yayJpg from '../assets/yay.jpg';
+import axios from 'axios';
+import { useEffect } from 'react'
+
+export default function List() {
+  useEffect(() => {
+    axios.get('/list').then(res => {
+      console.log(res);
+    })
+  }, [])
+  return (
+    <div>
+      列表页内容
+    </div>
+  );
+}
